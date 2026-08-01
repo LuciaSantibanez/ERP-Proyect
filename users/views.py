cc77a455a6dcc560e38d584069637969d0641e70
Lucia Santibañez <lucis@lusantiba ez>
2026-07-31 23:34:10 -0300

    Configuración inicial del ERP

---
+++ users/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import LoginForm
+from .models import UserRole, Role
+
+
+
